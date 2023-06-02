2766191019b902eeb1f33f8bcf22a81a6add5c62
nevil mistry <nevilmistry26@gmail.com>
2023-06-02 18:10:23 +0530

    fix : added cors options

--- server/Index.js
+++ server/Index.js
@@ -1,33 +1,37 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const database = require("./Database/db");
-var morgan = require("morgan");
-const model = require("./Model/empSchema");
-var cors = require("cors");
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const database = require("./Database/db")
+var morgan = require("morgan")
+var cors = require("cors")
 
 //morgan for api req
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
-//Database connection
-database.dbConnection;
+app.use(morgan("dev"))
 
-app.use("/emp", require("./Routers/empRoute"));
+const corsOptions = {
+  origin: "http://localhost:3000",
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+//Database connection
+database.dbConnection
+
+app.use("/emp", require("./Routers/empRoute"))
 
 app.get("/", (req, res) => {
-  res.send("hello");
-});
+  res.send("hello")
+})
 
 process.on("uncaughtException", function (err) {
-  console.log(err);
-});
+  console.log(err)
+})
 
-PORT = process.env.PORT;
+PORT = process.env.PORT
 
 //server
 app.listen(PORT, (err) => {
   err
     ? console.log(`error in server ${err} `)
-    : console.log(`Server started on port no ${PORT}`);
-});
+    : console.log(`Server started on port no ${PORT}`)
+})
--- server/Routers/empRoute.js
+++ server/Routers/empRoute.js
@@ -1,59 +1,60 @@
-const express = require("express");
-const router = express.Router();
-const emp = require("../Model/empSchema");
+const express = require("express")
+const router = express.Router()
+const emp = require("../Model/empSchema")
 
 router.get("/read", async (req, res) => {
   try {
-    const data = await emp.find({});
-    res.status(200).send(data);
+    const data = await emp.find({})
+    res.status(200).send(data)
   } catch (error) {
-    console.log("Error in read " + error);
-    res.status(400).send({ message: error });
+    console.log("Error in read " + error)
+    res.status(400).send({ message: error })
   }
-});
+})
 
 router.post("/insert", async (req, res) => {
-  const { name, email, password, dept } = req.body;
+  const { name, email, password, dept } = req.body
   try {
     const empData = new emp({
       name,
       email,
       password,
       dept,
-    });
-    await empData.save();
-    res.status(200).send(empData);
+    })
+    await empData.save()
+    res.status(200).send(empData)
   } catch (error) {
-    console.log("Error in inserted data " + error);
-    res.status(400).send({ message: error });
+    console.log("Error in inserted data " + error)
+    res.status(400).send({ message: error })
   }
-});
+})
 
 router.delete("/delete", async (req, res) => {
-  const { id } = req.body;
+  const { id } = req.body
   try {
-    const response = await emp.deleteOne({ id });
-    res.status(200).send(response);
+    const response = await emp.deleteOne({ id })
+    res.status(200).send(response)
   } catch (error) {
-    console.log("error in deleting emp" + error);
-    res.status(400).send({ message: error });
+    console.log("error in deleting emp" + error)
+    res.status(400).send({ message: error })
   }
-});
+})
 
 router.post("/update", async (req, res) => {
-  const { id, name, email, password, dept } = req.body;
+  const { id, name, email, password, dept } = req.body
   try {
-    const data = await emp.findOne({ _id: id });
-    data.name = name;
-    data.email = email;
-    data.password = password;
-    data.dept = dept;
-    await data.save();
-    res.status(200).send(data);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send({ message: error });
-  }
-});
+    const data = await emp.findOne({ _id: id })
+    data.name = name
+    data.email = email
+    data.password = password
+    data.dept = dept
 
-module.exports = router;
+    await data.save()
+    res.status(200).send(data)
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({ message: error })
+  }
+})
+
+module.exports = router
